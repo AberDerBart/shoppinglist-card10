0c5fbb90a44254415efbce420d3c3c3f09785686
AberDerBart <nonatz@web.de>
2019-08-21 23:59:53 +0200

    implement deleting items

--- __init__.py
+++ __init__.py
@@ -13,15 +13,27 @@ class SList:
 
         self.items = self.data['currentState']['items']
 
+    def removeItem(self, index):
+        self.items.pop(index)
+
 class Ui:
     offset = 0
     highlight = 0
-    pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT)
+    pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT |
+      buttons.TOP_RIGHT)
     lastPressed = pressed
 
     disp = display.open()
 
 def updateDisplay(slist):
+    
+    # adjust offset
+    if Ui.highlight < Ui.offset:
+        Ui.offset = Ui.highlight
+    if Ui.highlight - Ui.offset > 3:
+        Ui.offset = Ui.highlight - 3
+
+    # display list
     Ui.disp.clear()
 
     for row, item in enumerate(slist.items[Ui.offset:4+Ui.offset]):
@@ -37,20 +49,21 @@ def risingFlank(button):
 
 def updateButtons(slist):
     Ui.lastPressed = Ui.pressed
-    Ui.pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT)
+    Ui.pressed = buttons.read(buttons.BOTTOM_LEFT | buttons.BOTTOM_RIGHT |
+      buttons.TOP_RIGHT)
 
     if risingFlank(buttons.BOTTOM_LEFT):
         Ui.highlight -= 1
         if Ui.highlight < 0:
             Ui.highlight = 0
-        if Ui.highlight < Ui.offset:
-            Ui.offset = Ui.highlight
     if risingFlank(buttons.BOTTOM_RIGHT):
         Ui.highlight += 1
         if Ui.highlight >= len(slist.items):
             Ui.highlight = len(slist.items) - 1
-        if Ui.highlight - Ui.offset > 3:
-            Ui.offset = Ui.highlight - 3
+    if risingFlank(buttons.TOP_RIGHT) and len(slist.items):
+        slist.removeItem(Ui.highlight)
+        if Ui.highlight >= len(slist.items):
+            Ui.highlight = len(slist.items) - 1
 
 slist = SList('shoppinglist.json')
 
