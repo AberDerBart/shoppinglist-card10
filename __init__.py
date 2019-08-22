e5bd1e268977adedfa3b1d1201a58733166ff79c
AberDerBart <nonatz@web.de>
2019-08-22 14:02:09 +0200

    improve display of categories

--- __init__.py
+++ __init__.py
@@ -2,6 +2,7 @@ import utime
 import ujson
 import display
 import buttons
+import color
 
 
 class SList:
@@ -12,9 +13,21 @@ class SList:
         slFile.close()
 
         self.items = self.data['currentState']['items']
+        self.categories = {cat['id']: cat.get('color', '#ffffff')
+          for cat in self.data['categories']}
+
+        self.colors = []
+        for index, item in enumerate(self.items):
+            colorString = self.categories.get(
+              item.get('category'), '#ffffff')
+            r = int(colorString[1:3], 16)
+            g = int(colorString[3:5], 16)
+            b = int(colorString[5:7], 16)
+            self.colors.append((r, g, b))
 
     def removeItem(self, index):
         self.items.pop(index)
+        self.colors.pop(index)
         self.write()
 
     def write(self):
@@ -43,11 +56,14 @@ def updateDisplay(slist):
 
     if slist.items:
         for row, item in enumerate(slist.items[Ui.offset:4+Ui.offset]):
+            color = slist.colors[row+Ui.offset]
+            Ui.disp.rect(0, row * 20, 6, (row + 1) * 20, col=color)
             if row + Ui.offset == Ui.highlight:
                 itemString = item['name'] + ' ' * (12 - len(item['name']))
-                Ui.disp.print(itemString, posy=20*row, fg=(0,0,0), bg=(255,255,255))
+                Ui.disp.print(itemString, posy=20*row, posx=6, fg=(0,0,0),
+                  bg=(255,255,255))
             else:
-                Ui.disp.print(item['name'], posy=20*row)
+                Ui.disp.print(item['name'], posy=20*row, posx=6)
     else:
         Ui.disp.print('(EMPTY)', posy=30, posx=30)
 
@@ -74,12 +90,13 @@ def updateButtons(slist):
         if Ui.highlight >= len(slist.items):
             Ui.highlight = len(slist.items) - 1
 
-slist = SList('shoppinglist.json')
 
-def mainLoop():
+def mainLoop(slist):
     while(True):
         updateDisplay(slist)
         updateButtons(slist)
         utime.sleep_ms(50)
 
-mainLoop()
+if __name__ == '__main__':
+    slist = SList('shoppinglist.json')
+    mainLoop(slist)
